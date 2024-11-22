5f55f47871a5b5fa4793af931036d6351ae65201
timotiusdwiyuliantomalau <timotiusdym17@gmail.com>
2024-11-22 22:23:48 +0700

    Update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,20 +4,24 @@ import {
   ThemeProvider,
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { Stack, Tabs } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { View } from "react-native";
+import NavigationBar from "./header-footer/header";
+import TabBar from "./header-footer/footer";
+import AuthScreen from "./auth";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const [isLogin, setIsLogin] = React.useState(false);
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
@@ -33,13 +37,23 @@ export default function RootLayout() {
   }
 
   return (
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="detail/index" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
-        </Stack>
-        <StatusBar style="auto" />
-      </ThemeProvider>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      {!isLogin ? (
+        <AuthScreen></AuthScreen>
+      ) : (
+        <>
+          <NavigationBar></NavigationBar>
+          <Stack>
+            <Stack.Screen name="search" options={{ headerShown: false }} />
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="detail" options={{ headerShown: false }} />
+            <Stack.Screen name="cart" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+          <TabBar></TabBar>
+        </>
+      )}
+      <StatusBar style="auto" />
+    </ThemeProvider>
   );
 }
